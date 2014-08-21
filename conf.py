d83040deca3a953dfb7db62910bbd4cabf16c6d4
Johan Vervloet <johan.vervloet@gmail.com>
2014-08-21 22:35:00 +0200

    Closes #12 - Google analytics.

--- conf.py
+++ conf.py
@@ -591,6 +591,19 @@ SOCIAL_BUTTONS_CODE = """
 # </script>
 # """
 
+BODY_END = """
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-20752330-4', 'auto');
+  ga('send', 'pageview');
+
+</script>
+"""
+
 # EXTRA_HEAD_DATA = """
 # <link rel="stylesheet" type="text/css" href="/assets/css/tipuesearch.css">
 # <div id="tipue_search_content" style="margin-left: auto; margin-right: auto; padding: 20px;"></div>
