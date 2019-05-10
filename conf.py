0c8e17058893a81c3469ae5ec1993b5f6514cf4b
Johan Vervloet <johan.vervloet@gmail.com>
2019-05-10 22:13:29 +0200

    A docker container for gitlab-ci.

--- conf.py
+++ conf.py
@@ -648,7 +648,7 @@ REDIRECTIONS = []
 # }
 
 DEPLOY_COMMANDS = {'default': [
-    'rsync -pave ssh --delete output/ johanv@mijnvps:/var/www/vhosts/blog.johanv.org/',
+    "rsync -pave ssh --delete output/ $TARGET_LOCATION",
 ]}
 
 # github_deploy configuration
